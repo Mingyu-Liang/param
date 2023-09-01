11134f9b20517d757b889e3cd632b6de8e598625
Mingyu-Liang <mingyuliang1996@gmail.com>
2023-09-01 17:02:12 -0400

    Update analysis code

--- train/compute/python/tools/trace_analyze.py
+++ train/compute/python/tools/trace_analyze.py
@@ -9,6 +9,14 @@ def read_dictionary_from_json(file_path):
         return data
 
 
+def extract_events_from_specific_iteration(trace_events, iteration_name):
+    iteration = [event for event in trace_events if 'name' in event and event['name'] == iteration_name]
+    assert(len(iteration) == 1)
+    sorted_trace_events = sorted(trace_events, key=lambda kv: kv['ts'])
+    trace_events_under_iteration = [event for event in sorted_trace_events if event['ts'] > iteration[0]['ts'] and event['ts'] < iteration[0]['ts'] + iteration[0]['dur']]
+    return trace_events_under_iteration
+
+
 def analyze_iteration(trace_events):
     cpu_ops = [event for event in trace_events if 'cat' in event and (event['cat'] == 'cpu_op' or event['cat'] == 'user_annotation')]
     cpu_ops = sorted(cpu_ops, key=lambda kv: kv['ts'])
@@ -37,8 +45,11 @@ def analyze_iteration(trace_events):
             end_ts = max(end_ts, op['ts'] + op['dur'])
         segments_duration[key] = (end_ts - start_ts)/ 1000.0
 
-    # print(segments_duration)
-    print(f'Avg: {statistics.mean(segments_duration.values())}, std: {statistics.stdev(segments_duration.values())}, max: {max(segments_duration.values())}, min: {min(segments_duration.values())}')
+    print("Avg: {:.2f}\tstd: {:.2f}\tmax: {:.2f}\tmin: {:.2f}".format(statistics.mean(segments_duration.values()), 
+        statistics.stdev(segments_duration.values()),
+        max(segments_duration.values()),
+        min(segments_duration.values())
+    ))
 
 
 def analyze_nccl(trace_events):
@@ -49,9 +60,12 @@ def analyze_nccl(trace_events):
             nccl_kernels_durations[kernel['name']] = []
         nccl_kernels_durations[kernel['name']].append(kernel['dur'] / 1000.0)
 
-    # print(nccl_kernels_durations.keys())
     for key, value in nccl_kernels_durations.items():
-        print(key, ' avg: ', statistics.mean(value), ' std: ', statistics.stdev(value), ' max: ', max(value), ' min: ', min(value))
+        avg_value = statistics.mean(value)
+        std_value = statistics.stdev(value)
+        max_value = max(value)
+        min_value = min(value)
+        print(f"{key} avg: {avg_value:.2f}\tstd: {std_value:.2f}\tmax: {max_value:.2f}\tmin: {min_value:.2f}")
 
 
 def analyze_cpu_ops_duration(trace_events):
@@ -70,22 +84,26 @@ def analyze_cpu_ops_duration(trace_events):
             else:
                 cpu_ops[event['name']]['duration'].append(event['dur'])    
 
-    print(event_categories)
-    print(cpu_ops.keys())
-
     cpu_ops_durations = {}
     for cpu_op in cpu_ops:
         cpu_ops_durations[cpu_op] = sum(cpu_ops[cpu_op]['duration'])
 
     cpu_ops_duration_total = sum(cpu_ops_durations.values())
     sorted_cpu_ops_durations = sorted(cpu_ops_durations.items(), key=lambda kv: kv[1], reverse=True)
-    for cpu_op in sorted_cpu_ops_durations:
-        print(cpu_op, ' fraction: ', cpu_op[1]/cpu_ops_duration_total)
 
-    top10_cpu_ops = sorted_cpu_ops_durations[:10]
-    print('top 10 cpu ops duration fraction: ', sum([cpu_op[1] for cpu_op in top10_cpu_ops])/cpu_ops_duration_total)
-    top20_cpu_ops = sorted_cpu_ops_durations[:20]
-    print('top 20 cpu ops duration fraction: ', sum([cpu_op[1] for cpu_op in top20_cpu_ops])/cpu_ops_duration_total)
+    print("CPU op\tFraction\tAverage duration")
+    for cpu_op in sorted_top_cpu_ops_durations[:5]:
+        fraction = cpu_op[1] / top_cpu_ops_duration_total * 100
+        avg = cpu_op[1] / len(top_cpu_ops[cpu_op[0]]['duration']) / 1000.0
+        print("{}\t{:.2f}\t{:.2f}".format(cpu_op[0], fraction, avg))
+
+    top10_top_cpu_ops = sorted_top_cpu_ops_durations[:10]
+    top10_fraction = sum(cpu_op[1] for cpu_op in top10_top_cpu_ops) / top_cpu_ops_duration_total
+    print("top 10 top cpu ops duration fraction: {:.2f}".format(top10_fraction))
+
+    top20_top_cpu_ops = sorted_top_cpu_ops_durations[:20]
+    top20_fraction = sum(cpu_op[1] for cpu_op in top20_top_cpu_ops) / top_cpu_ops_duration_total
+    print("top 20 top cpu ops duration fraction: {:.2f}".format(top20_fraction))
 
 
 def analyze_top_cpu_ops(trace_events):
@@ -115,81 +133,21 @@ def analyze_top_cpu_ops(trace_events):
     top_cpu_ops_duration_total = sum(top_cpu_ops_durations.values())
     sorted_top_cpu_ops_durations = sorted(top_cpu_ops_durations.items(), key=lambda kv: kv[1], reverse=True)
     
-    for cpu_op in sorted_top_cpu_ops_durations:
-        print(cpu_op, ' fraction: ', cpu_op[1]/top_cpu_ops_duration_total, ' avg: ', cpu_op[1] / len(top_cpu_ops[cpu_op[0]]['duration']) / 1000.0)
+    print("Top CPU op\tFraction\tAverage duration")
+    for cpu_op in sorted_top_cpu_ops_durations[:5]:
+        fraction = cpu_op[1] / top_cpu_ops_duration_total * 100
+        avg = cpu_op[1] / len(top_cpu_ops[cpu_op[0]]['duration']) / 1000.0
+        print("{}\t{:.2f}\t{:.2f}".format(cpu_op[0], fraction, avg))
 
-    print('Total cpu ops duration: ', top_cpu_ops_duration_total)
+    print("Total cpu ops duration: {:.2f}".format(top_cpu_ops_duration_total))
+
     top10_top_cpu_ops = sorted_top_cpu_ops_durations[:10]
-    print('top 10 top cpu ops duration fraction: ', sum([cpu_op[1] for cpu_op in top10_top_cpu_ops])/top_cpu_ops_duration_total)
+    top10_fraction = sum(cpu_op[1] for cpu_op in top10_top_cpu_ops) / top_cpu_ops_duration_total
+    print("top 10 top cpu ops duration fraction: {:.2f}".format(top10_fraction))
+
     top20_top_cpu_ops = sorted_top_cpu_ops_durations[:20]
-    print('top 20 top cpu ops duration fraction: ', sum([cpu_op[1] for cpu_op in top20_top_cpu_ops])/top_cpu_ops_duration_total)
-
-        
-def analyze_cpu_ops_duration_under_specific_iteration(trace_events, iteration_name):
-    iteration = [event for event in trace_events if 'name' in event and event['name'] == iteration_name]
-    assert(len(iteration) == 1)
-    sorted_trace_events = sorted(trace_events, key=lambda kv: kv['ts'])
-    selected_trace_events = [event for event in sorted_trace_events if event['ts'] > iteration[0]['ts'] and event['ts'] < iteration[0]['ts'] + iteration[0]['dur']]
-    
-    cpu_events = [event for event in selected_trace_events if 'cat' in event and event['cat'] == 'cpu_op']
-    cpu_ops = {}
-
-    for event in cpu_events:
-        if event['name'] not in cpu_ops:
-            cpu_ops[event['name']] = {}
-            cpu_ops[event['name']]['duration'] = []
-            cpu_ops[event['name']]['duration'].append(event['dur'])
-        else:
-            cpu_ops[event['name']]['duration'].append(event['dur'])    
-
-    cpu_ops_durations = {}
-    for cpu_op in cpu_ops:
-        cpu_ops_durations[cpu_op] = sum(cpu_ops[cpu_op]['duration'])
-
-    cpu_ops_duration_total = sum(cpu_ops_durations.values())
-    sorted_cpu_ops_durations = sorted(cpu_ops_durations.items(), key=lambda kv: kv[1], reverse=True)
-
-    # for cpu_op in sorted_cpu_ops_durations:
-    #     print(cpu_op, ' fraction: ', cpu_op[1]/cpu_ops_duration_total)
-
-    top10_cpu_ops = sorted_cpu_ops_durations[:10]
-    print(f'top 10 cpu ops duration fraction under {iteration_name}: ', sum([cpu_op[1] for cpu_op in top10_cpu_ops])/cpu_ops_duration_total)
-    top20_cpu_ops = sorted_cpu_ops_durations[:20]
-    print(f'top 20 cpu ops duration fraction under {iteration_name}: ', sum([cpu_op[1] for cpu_op in top20_cpu_ops])/cpu_ops_duration_total)
-
-
-def analyze_top_cpu_ops_under_specific_iteration(trace_events, iteration_name):
-    iteration = [event for event in trace_events if 'name' in event and event['name'] == iteration_name]
-    assert(len(iteration) == 1)
-    sorted_trace_events = sorted(trace_events, key=lambda kv: kv['ts'])
-
-    cpu_events = [event for event in sorted_trace_events if 'cat' in event and event['cat'] == 'cpu_op']
-    top_cpu_events = [cpu_events[0]]
-    for event in cpu_events[1:]:
-        if event['ts'] < top_cpu_events[-1]['ts'] + top_cpu_events[-1]['dur']:
-            continue
-        else:
-            top_cpu_events.append(event)
-
-    top_cpu_events_under_iteration = [event for event in top_cpu_events if event['ts'] > iteration[0]['ts'] and event['ts'] < iteration[0]['ts'] + iteration[0]['dur']]
-    top_cpu_events_under_iteration_total_duration = sum([event['dur'] for event in top_cpu_events_under_iteration])
-
-    aten_events = [event for event in sorted_trace_events if 'cat' in event and event['cat'] == 'cpu_op' and event['name'].startswith('aten')]
-    top_aten_events = [aten_events[0]]
-    for event in aten_events[1:]:
-        if event['ts'] < top_aten_events[-1]['ts'] + top_aten_events[-1]['dur']:
-            continue
-        else:
-            top_aten_events.append(event)
-    
-    top_aten_events_under_iteration = [event for event in top_aten_events if event['ts'] > iteration[0]['ts'] and event['ts'] < iteration[0]['ts'] + iteration[0]['dur']]
-    top_aten_events_under_iteration_total_duration = sum([event['dur'] for event in top_aten_events_under_iteration])
-
-    print(f'top cpu events under {iteration_name} total duration: ', top_cpu_events_under_iteration_total_duration,
-          f'top aten events under {iteration_name} total duration: ', top_aten_events_under_iteration_total_duration,
-        f', {iteration_name} total duration: ', iteration[0]['dur'], 
-        ', cpu events fraction: ', top_cpu_events_under_iteration_total_duration / iteration[0]['dur'],
-        ', aten events fraction: ', top_aten_events_under_iteration_total_duration / iteration[0]['dur'])
+    top20_fraction = sum(cpu_op[1] for cpu_op in top20_top_cpu_ops) / top_cpu_ops_duration_total
+    print("top 20 top cpu ops duration fraction: {:.2f}".format(top20_fraction))
 
 
 def analyze_kernels_duration(trace_events):
@@ -208,27 +166,29 @@ def analyze_kernels_duration(trace_events):
             else:
                 kernels[event['name']]['duration'].append(event['dur'])    
 
-    print(event_categories)
-    print(kernels.keys())
-
     kernels_durations = {}
     for kernel in kernels:
         kernels_durations[kernel] = sum(kernels[kernel]['duration'])
 
     kernels_duration_total = sum(kernels_durations.values())
     sorted_kernels_durations = sorted(kernels_durations.items(), key=lambda kv: kv[1], reverse=True)
-    for kernel in sorted_kernels_durations:
-        print(kernel, ' fraction: ', kernel[1]/kernels_duration_total)
+    print("Kernel name\tFraction\tAverage duration")
+    for kernel in sorted_kernels_durations[:10]:
+        print("{}\t{:.2f}\t{:.2f}".format(
+            kernel[0], 
+            kernel[1] / kernels_duration_total * 100, 
+            kernel[1] / len(kernels[kernel[0]]['duration']) / 1000.0
+        ))
 
     top10_cpu_ops = sorted_kernels_durations[:10]
-    print('top 10 kernels duration fraction: ', sum([cpu_op[1] for cpu_op in top10_cpu_ops])/kernels_duration_total)
+    print("top 10 kernels duration fraction: {:.2f}".format(sum(cpu_op[1] for cpu_op in top10_cpu_ops) / kernels_duration_total))
+
     top20_cpu_ops = sorted_kernels_durations[:20]
-    print('top 20 kernels duration fraction: ', sum([cpu_op[1] for cpu_op in top20_cpu_ops])/kernels_duration_total)
+    print("top 20 kernels duration fraction: {:.2f}".format(sum(cpu_op[1] for cpu_op in top20_cpu_ops) / kernels_duration_total))
 
 
 # Example usage
-file_path = '/zhang-x3/users/ml2585/eg_logs/resnet_dist_4_long/resnet_dist_trace_1.json'  # Replace with your JSON file path
-# file_path = '/tmp/tmp_20230530_21b8d82_287757.json'
+file_path = '/zhang-x3/users/ml2585/eg_logs/resnet_dist_1/resnet_dist_trace_0.json'  # Replace with your JSON file path
 dictionary = read_dictionary_from_json(file_path)
 print(dictionary.keys())
 
@@ -239,23 +199,13 @@ analyze_nccl(trace_events)
 
 analyze_top_cpu_ops(trace_events)
 
+analyze_kernels_duration(trace_events)
+
+trace_events = extract_events_from_specific_iteration(trace_events, 'ProfilerStep#28')
+analyze_top_cpu_ops(trace_events)
+
 exit(1)
 
-# analyze_cpu_ops_duration(trace_events)
-
-# analyze_cpu_ops_duration_under_specific_iteration(trace_events, 'iteration#17')
-
-# analyze_top_cpu_ops_under_specific_iteration(trace_events, 'iteration#17')
-
-# analyze_kernels_duration(trace_events)
-
-iteration = [event for event in trace_events if 'name' in event and event['name'] == 'iteration#17']
-assert(len(iteration) == 1)
-sorted_trace_events = sorted(trace_events, key=lambda kv: kv['ts'])
-
-trace_events_under_iteration = [event for event in sorted_trace_events if event['ts'] > iteration[0]['ts'] and event['ts'] < iteration[0]['ts'] + iteration[0]['dur']]
-
-print(len(trace_events_under_iteration))
 
 cuda_runtime = [event for event in trace_events_under_iteration if event['cat'] == 'cuda_runtime']
 # kernels = [event for event in trace_events_under_iteration if event['cat'] == 'kernel']
